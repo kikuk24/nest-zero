721144debd8bb08d6fcaa3248bc14c71eebb976a
kikuk24 <tirta.afandi24@gmail.com>
2025-06-29 23:56:34 +0700

    feat: add endpoint user register

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { WebResponse } from "src/model/web.model";
+import { RegisterUserRequest, UserResponse } from "src/model/user.model";
 
-@Controller()
-export class UserController {}
+@Controller("api/user")
+export class UserController {
+    constructor(private userService: UserService) {
+
+    }
+    @Post("register")
+    async register(
+        @Body() request: RegisterUserRequest
+    ): Promise<WebResponse<UserResponse>> {
+        const result = await this.userService.registerUser(request);
+        return {
+            data: result,
+            message: "User registered successfully",
+        };
+    }
+}
