e3e2d60cf3df722db4aa7b3efd3bae2a6e62f2a9
kikuk24 <tirta.afandi24@gmail.com>
2025-06-29 22:44:22 +0700

    feat: add user validation

---
+++ src/user/user.validation.ts
@@ -0,0 +1,11 @@
+import { z, ZodType } from "zod";
+
+export class UserValidation {
+    static readonly REGISTER: ZodType = z.object({
+        email: z.string().email("Invalid email format"),
+        password: z.string().min(6, "Password must be at least 6 characters long"),
+        name: z.string().min(1, "Name is required").optional(),
+        provider: z.enum(["LOCAL", "GOOGLE", "FACEBOOK", "GITHUB"]).default("LOCAL").optional(),
+        isActive: z.boolean().default(true).optional(),
+    });
+}
